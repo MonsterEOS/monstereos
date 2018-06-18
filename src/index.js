d14cfa52737939ba27cb243fa26ab57d20d19bc3
leordev <leordev@gmail.com>
2018-06-17 23:36:11 -0400

    mainnet release

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import registerServiceWorker from './registerServiceWorker';
 import Eos from 'eosjs'
 
 const STORAGE_KEY = 'MONSTEREOS'
-const CHAIN_PROTOCOL = 'http'
-const CHAIN_HOST = '127.0.0.1'
-const CHAIN_PORT = 8888
+const CHAIN_PROTOCOL = 'https'
+const CHAIN_HOST = 'api.cypherglass.com'
+const CHAIN_PORT = 443
 const CHAIN_ADDRESS = CHAIN_PROTOCOL + '://' + CHAIN_HOST + ':' + CHAIN_PORT
-const CHAIN_ID = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f' //'7d47aae09c97dbc21d52c6d9f17bb70a1f1f2fda5f81b3ef18979b74b2070d8c'
-const MONSTERS_ACCOUNT = 'monstereosio' //'monstereosi1'
+const CHAIN_ID = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+const MONSTERS_ACCOUNT = 'monstereosio'
 const MONSTERS_TABLE = 'pets'
 const CONFIG_TABLE = 'petconfig'
 const BALANCES_TABLE = 'accounts'
