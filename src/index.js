b5356156d116176c083229cc0fd822698fc03ed9
leordev <leordev@gmail.com>
2018-06-28 07:16:05 -0400

    uses scatter nodes balancer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import ecc from 'eosjs-ecc'
 
 const STORAGE_KEY = 'MONSTEREOS'
 const CHAIN_PROTOCOL = 'http'
-const CHAIN_HOST = 'br.eosrio.io' //'mainnet.eoscalgary.io'
-const CHAIN_PORT = '8080' //80
+const CHAIN_HOST = 'nodes.get-scatter.com' //'br.eosrio.io' //'mainnet.eoscalgary.io'
+const CHAIN_PORT = '80' //8080' //80
 const CHAIN_ADDRESS = CHAIN_PROTOCOL + '://' + CHAIN_HOST + ':' + CHAIN_PORT
 const CHAIN_ID = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 const MONSTERS_ACCOUNT = 'monstereosio'
