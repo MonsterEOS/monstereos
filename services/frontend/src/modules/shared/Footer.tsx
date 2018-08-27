71a861caa21e2334d2815a1a455d717144b60a5e
John Williamson <johnjwilliamson@live.com>
2018-08-28 09:54:19 +1000

    added twitter and medium icons to footer

--- services/frontend/src/modules/shared/Footer.tsx
+++ services/frontend/src/modules/shared/Footer.tsx
@@ -17,6 +17,16 @@ const Footer = (props: any) => (
             <i className="fa fa-2x fa-github" style={{verticalAlign: "middle", marginRight: 3}} />
             GitHub
           </a>
+          {" "}
+          <a href="https://medium.com/@monstereos" target="_blank">
+            <i className="fa fa-2x fa-medium" style={{verticalAlign: "middle", marginRight: 3}} />
+            Medium
+          </a>
+          {" "}
+          <a href="https://twitter.com/MonsterEos" target="_blank">
+            <i className="fa fa-2x fa-twitter" style={{verticalAlign: "middle", marginRight: 3}} />
+            Twitter
+          </a>
         </p>
       </div>
     </div>
