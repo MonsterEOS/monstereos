65063db80661562fadd8c16eb437811b309e5337
Leo Ribeiro <leo.ribeiro@block.one>
2018-09-05 09:31:50 -0400

    wallet creation script 1.2

--- services/eos-dev/scripts/0000_init-chain.sh
+++ services/eos-dev/scripts/0000_init-chain.sh
@@ -58,7 +58,7 @@ sleep 2s
 echo "Creating accounts and deploying wallets"
 
 # start wallet
-wallet_password=$(./cleos wallet create | awk 'FNR > 3 { print $1 }' | tr -d '"')
+wallet_password=$(./cleos wallet create --to-console | awk 'FNR > 3 { print $1 }' | tr -d '"')
 echo $wallet_password > "$CONFIG_DIR"/keys/default_wallet_password.txt
 
 # import wallet keys
