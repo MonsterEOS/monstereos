426db42f468e1e817e03f5afc94b63f0da271dfd
Leo Ribeiro <leo.ribeiro@block.one>
2018-09-06 18:05:06 -0400

    markets adjusts

--- services/eos-dev/scripts/tests-market.sh
+++ services/eos-dev/scripts/tests-market.sh
@@ -3,11 +3,15 @@
 MONSTERS_USERA_ACCOUNT="monsterusera"
 
 echo "make offer and remove it"
-cleos -u http://eosiodev:8888 push action monstereosmt offerpet '[1, "monsteruserb"]' -p monsterusera
-cleos -u http://eosiodev:8888 get table monstereosmt monstereosmt offers -l 1
+cleos -u http://eosiodev:8888 push action monstereosmt offerpet '[1, "", "1.0000 EOS"]' -p monsteruserb
+cleos -u http://eosiodev:8888 get table monstereosmt monstereosmt offers -l 100
+cleos -u http://eosiodev:8888 get table monstereosio monstereosio pets -l 100
 
 cleos -u http://eosiodev:8888 push action monstereosmt removeoffer '["monsterusera", 1]' -p monsterusera
-cleos -u http://eosiodev:8888 get table monstereosmt monstereosmt offers -l 1
+cleos -u http://eosiodev:8888 get table monstereosmt monstereosmt offers -l 100
+
+cleos -u http://eosiodev:8888 push action monstereosmt claimpet '["monsteruserb", 1, "monsterusera"]' -p monsterusera
+cleos -u http://eosiodev:8888 push action eosio.token transfer '["monsteruserb", "monstereosio", "0.0003 EOS", "MTT123"]' -p monsteruserb
 
 sleep .5
 
@@ -36,8 +40,11 @@ echo "one offers for monsteruserb"
 cleos -u http://eosiodev:8888 get table monstereosmt monstereosmt offers -l 1
 
 # add some money for buying monsters
-cleos -u http://eosiodev:8888 transfer eosio monsterusera "1.0000 EOS"
-cleos -u http://eosiodev:8888 transfer eosio monsteruserb "1.0000 EOS"
+cleos -u http://eosiodev:8888 transfer eosio monsterusera "1000.0000 EOS"
+cleos -u http://eosiodev:8888 transfer eosio monsteruserb "1000.0000 EOS"
+cleos -u http://eosiodev:8888 transfer eosio monsteruserc "1000.0000 EOS"
+cleos -u http://eosiodev:8888 transfer eosio monsteruserd "1000.0000 EOS"
+cleos -u http://eosiodev:8888 transfer eosio monsterusere "1000.0000 EOS"
 
 sleep .5
 cleos -u http://eosiodev:8888 push action eosio.token transfer '["monsteruserb", "monstereosio", "1.0000 EOS", "MTT0"]' -p monsteruserb
