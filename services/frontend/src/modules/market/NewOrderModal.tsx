fbf7930fdad05ff0ff6bed74ea6aa65db8f7fa38
Leo Ribeiro <leordev@gmail.com>
2018-09-15 10:32:20 -0400

    new order modal autocomplete to select dropdown

--- services/frontend/src/modules/market/NewOrderModal.tsx
+++ services/frontend/src/modules/market/NewOrderModal.tsx
@@ -1,10 +1,9 @@
 import * as React from "react"
-import { State, pushNotification, NOTIFICATION_SUCCESS, NOTIFICATION_ERROR, NOTIFICATION_WARNING } from "../../store"
+import { State, pushNotification, NOTIFICATION_SUCCESS, NOTIFICATION_ERROR } from "../../store"
 import { connect } from "react-redux"
 import { trxOrderPetMarket } from "../../utils/eos"
 import Modal from "../shared/Modal"
-import { MonsterProps, monsterImageSrc } from "../monsters/monsters"
-import * as AutoSuggest from "react-autosuggest"
+import { MonsterProps } from "../monsters/monsters"
 
 interface Props {
   closeModal: (doUpdate: boolean) => void,
@@ -36,9 +35,9 @@ class NewOrderModal extends React.Component<Props, {}> {
 
   public render() {
 
-    const { closeModal } = this.props
+    const { closeModal, monsters } = this.props
 
-    const { name, monstername, monster, amount, suggestedMonsters } = this.state
+    const { name, monster, amount } = this.state
 
     const footerButtons = [
       <button
@@ -57,17 +56,6 @@ class NewOrderModal extends React.Component<Props, {}> {
 
     const title =  this.props.initialMonster ? "Update Order" : "Create a New Sell Order"
 
-    const inputProps = {
-      placeholder: "Bubble",
-      value:monstername,
-      onChange: this.handleChangeMonster
-    }
-
-    const theme = {
-      container:"is-large",
-      input:"input is-large"
-    }
-
     return (
       <Modal
         title={title}
@@ -75,22 +63,22 @@ class NewOrderModal extends React.Component<Props, {}> {
         footerButtons={footerButtons}>
         <div>
           <div className="field">
-            <label className="label is-large">Monster Name</label>             
-            <div className="control has-icons-left has-icons-right">                    
-              <AutoSuggest theme={theme}
-                  suggestions={suggestedMonsters}
-                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                  getSuggestionValue={this.getMonsterSuggestionValue}
-                  renderSuggestion={this.renderMonsterSuggestion}
-                  inputProps={inputProps}
-                />
-              <span className="icon is-left">
-                <i className="fa fa-paw" />
-              </span>
-              {monster && <span className="label">
-                #{monster.id}
-              </span>}
+            <label className="label is-large">Monster Name</label>     
+            <div className="control has-icons-left">
+              <div className="select is-fullwidth">
+                <select 
+                  className="is-large" 
+                  onChange={this.handleChangeMonster}
+                  value={monster ? monster.id : ""}>
+                  <option value="" selected>Please select the Monster you want to Sell</option>
+                  {monsters.map((opt) => 
+                    <option 
+                      key={opt.id}
+                      value={opt.id}>{opt.name} (#{opt.id})</option> 
+                  )}
+                </select>
+              </div>
+              <span className="icon is-left"><i className="fa fa-paw" /></span>
             </div>
           </div>
           <div className="field">
@@ -134,20 +122,13 @@ class NewOrderModal extends React.Component<Props, {}> {
   }
 
   private handleChangeMonster = (event: any) => {
-    if (event.type === "change") {
-      const monsterName = event.target.value
-      const {dispatchPushNotification, monsters} = this.props
-      const monstersWithName = monsters.filter((monster:MonsterProps) => monsterName && monster.name.toLowerCase() === monsterName.toLowerCase())      
-      if (monstersWithName.length > 0) {              
-        if (monstersWithName.length > 1) {
-          dispatchPushNotification(`More than one monster found with this name. Using the first one`, NOTIFICATION_WARNING)
-        }  
-        this.setState({monstername:monsterName,
-          monster: monstersWithName[0]})
-      } else {    
-        this.setState({monstername:monsterName,
-          monster: undefined})      
-      }
+
+    const {monsters} = this.props
+
+    const monster = monsters.find((m) => m.id === Number(event.target.value))
+
+    if (monster) {
+      this.setState({monster})
     }
   }
 
@@ -178,40 +159,6 @@ class NewOrderModal extends React.Component<Props, {}> {
         console.error(`Fail to offer monster ${monster.id}`, err)
         dispatchPushNotification(`Fail to offer ${monster.name}`, NOTIFICATION_ERROR)
       })
-  }
-
-  private getMonsterSuggestionValue = (monster:MonsterProps) => {
-    this.setState({monster, monstername: monster.name})
-    return monster.name
-  }
-  
-  private renderMonsterSuggestion = (monster:MonsterProps) => {
-    return (
-    <div className="has-icons-left control">
-          <img src={monsterImageSrc(monster.type)} className="icon is-left"/>
-          <label className="label">{monster.name}</label>
-    </div>
-    )
-  }
-
-  private getSuggestedMonsters = (monsterName:string) => {    
-    if (monsterName && monsterName.length > 1) {
-      const {monsters} = this.props
-      return monsters.filter((monster) => monster.name.toLowerCase().indexOf(monsterName) >= 0)
-    } else {
-      return []
-    }
-  }
-  private onSuggestionsFetchRequested = ( event:AutoSuggest.SuggestionsFetchRequestedParams  ) => {    
-    this.setState({
-      suggestedMonsters: this.getSuggestedMonsters(event.value.toLowerCase())
-    })
-  }
-
-  private onSuggestionsClearRequested = () => {    
-    this.setState({
-      suggestedMonsters:[]
-    })
   }
 }
   
