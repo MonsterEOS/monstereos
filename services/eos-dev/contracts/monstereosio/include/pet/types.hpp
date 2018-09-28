a83ccbb6237fe8996b9f73db5545c4a79c6a953c
Leo Ribeiro <leo.ribeiro@block.one>
2018-09-28 19:05:55 -0400

    accounts beginning

--- services/eos-dev/contracts/monstereosio/include/pet/types.hpp
+++ services/eos-dev/contracts/monstereosio/include/pet/types.hpp
@@ -128,6 +128,15 @@ namespace types {
   };
   typedef multi_index<N(accounts), st_account> _tb_accounts;
 
+  // @abi table accounts2 i64
+  struct st_account2 {
+      name                      owner;
+      flat_map<symbol, int64_t> balances;
+
+      uint64_t primary_key() const { return owner; }
+  };
+  typedef multi_index<N(accounts2), st_account2> _tb_accounts2;
+
   // @abi table elements i64
   struct st_elements {
       uint64_t id;
