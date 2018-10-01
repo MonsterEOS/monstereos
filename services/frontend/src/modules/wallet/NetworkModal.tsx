0bdec0f5906fdb9ec5d34bca841e321e5d250194
marcelormendes <mmendes200@gmail.com>
2018-10-01 02:38:05 -0300

    Select Network Modal created

--- services/frontend/src/modules/wallet/NetworkModal.tsx
+++ services/frontend/src/modules/wallet/NetworkModal.tsx
@@ -1,43 +1,35 @@
 import * as React from "react"
-import { State, pushNotification, NOTIFICATION_SUCCESS, NOTIFICATION_ERROR } from "../../store"
-import { connect } from "react-redux"
-import { trxOrderPetMarket } from "../../utils/eos"
+import {setNetwork, State} from "../../store"
+import {connect} from "react-redux"
 import Modal from "../shared/Modal"
-import { MonsterProps } from "../monsters/monsters"
+import {Network, networks} from "./networks"
 
 interface Props {
   closeModal: (doUpdate: boolean) => void,
-  scatter: any,
-  dispatchPushNotification: any,
-  monsters: MonsterProps[],
-  initialName?: string,
-  initialMonster?: MonsterProps,
-  initialAmount?: number
+  dispatchSetNetwork: any
 }
 
 interface ReactState {
-  name: string,
-  monstername: string,
-  amount: number,
-  monster: MonsterProps | undefined
-  suggestedMonsters: MonsterProps[]
+  networkList: Network[],
+  id: string
 }
 
-class NewOrderModal extends React.Component<Props, {}> {
+class NetworkModal extends React.Component<Props, ReactState> {
 
   public state: ReactState = {
-    name: this.props.initialName ? this.props.initialName: "",
-    monstername: this.props.initialMonster ? this.props.initialMonster.name: "",
-    amount: this.props.initialAmount ? this.props.initialAmount : 0,
-    monster: this.props.initialMonster,
-    suggestedMonsters: []
+    networkList: networks,
+    id: "cypherglasss"
+  }
+
+  public componentDidMount() {
+    this.getNetwork()
   }
 
   public render() {
 
-    const { closeModal, monsters } = this.props
+    const {closeModal} = this.props
 
-    const { name, monster, amount } = this.state
+    const {networkList, id} = this.state
 
     const footerButtons = [
       <a
@@ -49,128 +41,89 @@ class NewOrderModal extends React.Component<Props, {}> {
       <a
         key="submit"
         className="button"
-        onClick={this.createOrder}>
+        onClick={this.setNetwork}>
         Submit
       </a>
     ]
 
-    const title =  this.props.initialMonster ? "Update Order" : "Create a New Sell Order"
-
     return (
       <Modal
-        title={title}
+        title="Select the Network"
         close={() => closeModal(false)}
         footerButtons={footerButtons}>
         <div>
           <div className="field">
-            <label className="label">Monster Name</label>
+            <label className="label">Network Name</label>
             <div className="control has-icons-left">
               <div className="select is-fullwidth">
                 <select
                   className="is-large"
-                  onChange={this.handleChangeMonster}
-                  value={monster ? monster.id : ""}>
-                  <option value="">Please select the Monster you want to Sell</option>
-                  {monsters.map((opt) =>
+                  onChange={this.handleChangeNetwork}
+                  value={id}>
+                  {networkList.map((opt) =>
                     <option
                       key={opt.id}
-                      value={opt.id}>{opt.name} (#{opt.id})</option>
+                      value={opt.id}>{opt.name} - Latency: {opt.ping}ms</option>
                   )}
                 </select>
               </div>
-              <span className="icon is-left"><i className="fa fa-paw" /></span>
+              <span className="icon is-left"><i className="fa fa-server"/></span>
             </div>
           </div>
-          <div className="field">
-            <label className="label">Value in EOS</label>
-            <div className="control has-icons-left has-icons-right">
-              <input
-                className="input is-large"
-                placeholder="0.0000"
-                type="number"
-                min="0.0000"
-                step="0.0001"
-                onChange={this.handleChangeValue}
-                defaultValue={amount ? Number(amount).toFixed(4) : ""}/>
-              <span className="icon is-left">
-                <i className="fa fa-money" />
-              </span>
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Propose to New Owner (optional)</label>
-            <div className="control has-icons-left has-icons-right">
-              <input
-                className="input is-large"
-                placeholder="friedgermuef"
-                type="text"
-                onChange={this.handleChangeName}
-                value={name} />
-              <span className="icon is-left">
-                <i className="fa fa-user" />
-              </span>
-            </div>
-          </div>
-
         </div>
       </Modal>
     )
   }
 
-  private handleChangeName = (event: any) => {
-    this.setState({name: event.target.value})
+  private getNetwork = async  () => {
+
+    const { networkList } = this.state
+
+    const initialTimeStamp = Date.now()
+
+    networkList.forEach(network => {
+      fetch(network.url)
+        .then(res => {
+          network.ping = Date.now() - initialTimeStamp
+          this.setNetworkState(networkList)
+        })
+        .catch(e => {
+          network.ping = 9999
+          this.setNetworkState(networkList)
+        })
+
+    })
+
+
   }
 
-  private handleChangeMonster = (event: any) => {
-
-    const {monsters} = this.props
-
-    const monster = monsters.find((m) => m.id === Number(event.target.value))
-
-    if (monster) {
-      this.setState({monster})
-    }
+  private setNetworkState = (networkList: Network[]) => {
+    networkList.sort((a, b) => {
+      return a.ping - b.ping
+    })
+    this.setState({networkList})
   }
 
-  private handleChangeValue = (event: any) => {
-    if (event.target.value) {
-      this.setState({amount: Number(event.target.value).toFixed(4)})
-    }
+  private handleChangeNetwork = (event: any) => {
+    this.setState({id: event.target.value})
   }
 
-  private createOrder = () => {
-    const { scatter, closeModal, dispatchPushNotification } = this.props
-    const { name, amount, monster } = this.state
-
-    if (!monster) {
-      return dispatchPushNotification(`Monster is required to make an offer`, NOTIFICATION_ERROR)
-    }
-
-    if (amount && amount < 0 ) {
-      return dispatchPushNotification(`Invalid amount for offer`, NOTIFICATION_ERROR)
-    }
-
-    trxOrderPetMarket(scatter, monster.id, name, amount)
-      .then((res: any) => {
-        console.info(`Pet ${monster.id} was offered to ${name} successfully`, res)
-        dispatchPushNotification(`Pet ${monster.name} was offered to ${name} successfully`, NOTIFICATION_SUCCESS)
-        closeModal(true)
-      }).catch((err: any) => {
-        dispatchPushNotification(`Fail to offer ${monster.name} ${err.eosError}`, NOTIFICATION_ERROR)
-      })
+  private setNetwork = () => {
+    const {dispatchSetNetwork} = this.props
+    dispatchSetNetwork(this.state.id)
   }
+
 }
 
 
 const mapStateToProps = (state: State) => {
   return {
     scatter: state.scatter,
-    monsters: state.myMonsters
   }
 }
 
 const mapDispatchToProps = {
-  dispatchPushNotification: pushNotification
+  dispatchSetNetwork: setNetwork
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewOrderModal)
+export default connect(mapStateToProps, mapDispatchToProps)(NetworkModal)
---
+++ services/frontend/src/modules/wallet/networks.ts
@@ -0,0 +1,109 @@
+export interface Network {
+    id: string,
+    name: string,
+    url: string,
+    ping: number
+}
+
+export const networks: Network[] = [
+    {
+        id: "cypherglasss",
+        name: "Cypherglass",
+        url: "https://api.cypherglass.com/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eosdacserver",
+        name: "eosDAC",
+        url: "https://eu.eosdac.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eoscafeblock",
+        name: "EOS Cafe",
+        url: "https://mainnet.eoscalgary.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "cryptolions1",
+        name: "Cryptolions",
+        url: "https://bp.cryptolions.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "zbeosbp11111",
+        name: "Zb Eos",
+        url: "https://node1.zbeos.com/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "starteosiobp",
+        name: "Starteos",
+        url: "http://api-mainnet.starteos.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eosriobrazil",
+        name: "EOS Rio",
+        url: "https://hapi.eosrio.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eos42freedom",
+        name: "EOS42",
+        url: "https://nodes.eos42.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eosauthority",
+        name: "EOS Authority",
+        url: "https://publicapi-mainnet.eosauthority.com/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "teamgreymass",
+        name: "Greymass",
+        url: " https://eos.greymass.com/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "atticlabeosb",
+        name: "AtticLab",
+        url: "https://eosbp.atticlab.net/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eoscanadacom",
+        name: "EOS Canada",
+        url: "https://mainnet.eoscanada.com/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eospaceioeos",
+        name: "EOSpace",
+        url: "https://api.eossweden.se/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eosswedenorg",
+        name: "EOS Sw/eden",
+        url: "https://api.eossweden.se/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "eosnewyorkio",
+        name: "EOS New York",
+        url: "https://api.eosnewyork.io/v1/chain/get_info",
+        ping: 0
+    },
+    {
+        id: "aus1genereos",
+        name: "Genereos",
+        url: "https://mainnet.genereos.io/v1/chain/get_info",
+        ping: 0
+    }
+
+]
+
+
+
