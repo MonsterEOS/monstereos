48306134923dd94f4c59b3d4c5cbf31f34878132
marcelormendes <mmendes200@gmail.com>
2018-10-21 17:34:26 -0300

    Monsters EOS - Translation

---
+++ services/frontend/src/setupTests.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var enzyme = require("enzyme");
+var Adapter = require("enzyme-adapter-react-16");
+enzyme.configure({ adapter: new Adapter() });
