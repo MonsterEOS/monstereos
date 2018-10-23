01aee3052ab9f9e423b9d8d5578e64f206493861
Gabo Esquivel <contact@gaboesquivel.com>
2018-10-23 06:55:42 -0600

    refactor: remove unnecessary node-start.sh file

--- services/eos-node/start-node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Starting fullnode ...'
-
-if [ "$(ls -A $DATA_DIR)" ]; then
-  /opt/eosio/bin/nodeosd.sh --data-dir $DATA_DIR --hard-replay 
-else
-  /opt/eosio/bin/nodeosd.sh --data-dir $DATA_DIR --genesis-json genesis.json --delete-all-blocks
-fi
